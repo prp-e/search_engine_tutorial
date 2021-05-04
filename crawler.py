af57f56a8f3b7e25b8ea4973f95a376db5868977
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-05 00:47:31 +0430

    starting the magic

---
+++ crawler.py
@@ -0,0 +1,9 @@
+# Steps for perfoming a web search 
+#   1. Crawl the web
+#   2. Index the material we just crawled. 
+#   3. Performing the search. 
+#
+# Crawler
+#   1. Opens a link 
+#   2. Searches for other links in the page
+#   3. Does the same for other links. 
