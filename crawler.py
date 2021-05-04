d0239f6ff48de709143c17099b382fcbf5e6e10f
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-05 00:52:32 +0430

    babysteps to crawl the web

--- crawler.py
+++ crawler.py
@@ -7,3 +7,18 @@
 #   1. Opens a link 
 #   2. Searches for other links in the page
 #   3. Does the same for other links. 
+
+from bs4 import BeautifulSoup 
+import requests
+
+def crawl(url):
+    try:
+        print(f'Crawling {url}')
+        response = requests.get(url)
+    except:
+        pass 
+
+    content = BeautifulSoup(url, 'lxml')
+    links = content.findAll('a')
+
+    print(len(links))
